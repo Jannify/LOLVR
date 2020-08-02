d7422b71efadfaeff39c843ca739a72f31a5ffeb
Jannify <Kannst-Du-mir-folgen@gmx.de>
2020-08-02 19:38:59 +0200

    Added TMPro and first version of transmitting keys

---
+++ Assets/Scripts/TransmittingKeys.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using WindowsInput;
+using WindowsInput.Native;
+using UnityEngine;
+
+public class TransmittingKeys : MonoBehaviour
+{
+    //InputSimulator simulator;
+
+    private void Start()
+    {
+        //simulator = new InputSimulator();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.K))
+        {
+            //simulator.KeyPress(VirtualKeyCode.VK_K);
+            //simulator.Keyboard.KeyPress(VirtualKeyCode.VK_0);
+            Process p = Process.GetProcessById(15184);
+            p.StandardInput.Write("Hello");
+        }
+    }
+}
