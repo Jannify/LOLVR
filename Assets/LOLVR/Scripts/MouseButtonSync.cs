d6f87b2bb4cd93a1680669bf3021973de68beeee
Jannify <23176718+Jannify@users.noreply.github.com>
2023-11-05 19:55:19 +0100

    Added HealthPotion and basic vr interaction

---
+++ Assets/LOLVR/Scripts/MouseButtonSync.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace LOLVR
+{
+    public class MouseButtonSync : MonoBehaviour
+    {
+        private void Update()
+        {
+            if (Input.GetKeyDown((KeyCode)ConfigManager.LeftClick)) SendAction.SimulateMouseLeftDown();
+            else if (Input.GetKeyDown((KeyCode)ConfigManager.RightClick)) SendAction.SimulateMouseRightDown();
+            if (Input.GetKeyUp((KeyCode)ConfigManager.LeftClick)) SendAction.SimulateMouseLeftUp();
+            else if (Input.GetKeyUp((KeyCode)ConfigManager.RightClick)) SendAction.SimulateMouseRightUp();
+        }
+    }
+}
